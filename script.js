91235e6eef294a930fd172a075e32de0dcdfe4ca
IvanBakanov <66953937+IvanBakanov@users.noreply.github.com>
2021-11-01 13:54:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const input = document.getElementsByTagName("input");
 const th = document.getElementsByTagName("th");
-const k = 1.5;
 
 document.forms.data.onsubmit = function () {
     event.preventDefault();
@@ -13,10 +12,10 @@ document.forms.data.onsubmit = function () {
     const W = arr[3] * arr[4] / arr[0];
 
     const M = arr[1] + arr[2];
-    const V = W * Math.log((arr[1] + M) / M) / k;
+    const V = W * Math.log((M + arr[0]) / M) / 1.5;
 
-    const h1 = V * arr[4] / (2 * k);
-    const h = h1 + arr[4] * (V - arr[4] * 4.9) / k;
+    const h1 = V * arr[4] / 2;
+    const h = h1 + arr[4] * (V - arr[4] * 4.9);
 
     arr = [V, W, h1, h];
     for (let i = 0; i < 4; i++) {
